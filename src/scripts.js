4f77062b859ecd27c100615fe35f92e39af6ebbb
Jeremy <jeremymiller00@gmail.com>
2019-06-08 08:28:17 -0700

    update files

--- src/scripts.js
+++ src/scripts.js
@@ -1,47 +1,60 @@
-
-//create a request variable for the connection
+// Set variable for the http handler
 var request = new XMLHttpRequest();
 
-//Open a new connection, using the GET request method
-request.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=cycling&fq=pub_year:("1998")&api-key=G0xUOLbiRaC4QtEyLoLPuIqgUP0AKMrj', true);
+// ===============================================
+// Variables for updating page & year in the query
+// ===============================================
+var pageNum = 1;
+var year = 1990;
 
-request.onload = function() {
+// =======================================
+// Initialize variable for timed API calls
+// =======================================
+var Id = 0;
 
-  //Begin accessing JSON data here
-  if (request.status >= 200 && request.status < 400) {
-    const data = JSON.parse(this.response);
+// ======================================================
+// Web URLs for each article will be stored in this array
+// ======================================================
+var webURLs = [];
+
+// =================
+// Make the API call
+// =================
+function makeRequest() {
+  request.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=cycling&page=${pageNum}&fq=pub_year:("${year}")&api-key=G0xUOLbiRaC4QtEyLoLPuIqgUP0AKMrj`, true);
+
+// ==========================
+// Parse the response payload
+// ==========================
+  request.onload = function() {
+    // console.log(this.response);
+    var data = JSON.parse(this.response);
     var docs = data.response.docs;
-  
-    for(i = 0; i < docs.length; i++) {
-      const app = document.getElementById('root');
-      
-      // Create a container
-      const container = document.createElement('div');
-      container.setAttribute('class', 'container');
-      app.appendChild(container);
-      
-      // Create a div with a card class
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
-      
-      //Create an h3 and set the text content to 'URL'
-      const h3 = document.createElement('h3');
-      h3.textContent = `URL`;
-      
-      //Create a p and set the text content to the URL
-      const p = document.createElement('p');
-      p.textContent = docs[i].web_url;
-      
-      //Append cards to the container
-      container.appendChild(card);
-      
-      //Each card will contain an h3 and a p
-      card.appendChild(h3);
-      card.appendChild(p);
-    
+    var offset = data.response.meta.offset;
+
+// ===========================================
+// Add each web_url value to the webURLs array
+// ===========================================
+    for( i = 0; i < docs.length; i++) {
+    webURLs.push(docs[i].web_url);
     }
-  } else {
-      console.log('error');
+    console.log(`The offset is ${offset}`);
+    // Increment the page number for the next API call
+    pageNum++;
   }
+  // Send the http request
+  request.send();
 }
-request.send();
+
+// =========================================================
+// Functions to start/stop the API call through the browswer
+// =========================================================
+function getArticles()
+{
+  Id = window.setInterval( function() { makeRequest(); }, 5000);
+}
+
+function stop()
+{
+  window.clearInterval(Id);
+}
